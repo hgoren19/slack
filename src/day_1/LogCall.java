22b6f8f825bb710386b41f2ae4c5cc0b8e86b302
hgoren19 <hgoren19@gmail.com>
2020-05-08 14:28:52 -0500

    add LogCall.java

---
+++ src/day_1/LogCall.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class LogCall {
+    public static void main(String[] args) {
+        System.out.println("Creating Log Call function is working");
+        //users are able to call through slack
+    }
+}
